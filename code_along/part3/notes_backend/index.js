651b7ff2d26e031e6b306b72f443331dc05fd5f3
Christian Timm <christian.timm@me.com>
2025-02-11 22:23:39 +0100

    add frontend build to backend part3

--- code_along/part3/notes_backend/index.js
+++ code_along/part3/notes_backend/index.js
@@ -49,6 +49,7 @@ const unknownEndpoint = (req, res) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 
--- code_along/part3/notes_frontend/src/services/notes.js
+++ code_along/part3/notes_frontend/src/services/notes.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/notes'
+// const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
+
 
 const getAll = () => {
     const nonExisting = {
