0b6d1319ee35b387934d6c9d01660ed699a67d10
Christian Timm <christian.timm@me.com>
2025-02-12 02:28:38 +0100

    added_mongodb_connection

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fullstackopen-2025
-2025 run off fullstackopen
+2025 run of fullstackopen
 
 - link to online notes app: https://render-test-l6in.onrender.com/
 - link to online phonebook app: https://phonebook-app-7ia1.onrender.com/
---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+// console.log('process.argv:', process.argv)
+// console.log('process.argv.length:', process.argv.length)
+
+if(process.argv.length < 3) {
+    console.log('No password given. Abort.')
+    process.exit(1)
+    
+}
+
+const username = encodeURIComponent("chris");
+const password = encodeURIComponent(process.argv[2])
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://${username}:${password}@fso-test.ageoo.mongodb.net/phonebook?retryWrites=true&w=majority&appName=fso-test`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 5) {
+    const person = new Person({
+        name,
+        number
+    })
+    
+    person.save().then(res => {
+        console.log(`added ${res.name} number ${res.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else if(process.argv.length === 3) {
+
+    Person.find({}).then(res => {
+        console.log(`all people (${res.length}):`)
+        res.forEach(person => console.log(person.name, person.number))
+        mongoose.connection.close()
+    })
+} else {
+    console.log('too many arguments. Abort.')
+    process.exit(1)
+}
